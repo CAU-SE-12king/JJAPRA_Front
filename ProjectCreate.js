2459205ed7e81864f5fe89465a473071cb8f62a0
suzzang2 <subin030102@naver.com>
2024-05-19 20:35:59 +0900

    Feat: 기억안남

--- ProjectCreate.js
+++ ProjectCreate.js
@@ -1,3 +1,22 @@
-function openProjectList() {
-    window.location.href = "./ProjectList.html"; //
+
+const openProjectList = () => {
+    // window.location.href = "./ProjectList.html"; // 기존 탭에서 전환
+    window.open("./ProjectList.html", "_blank"); // 새 탭으로 열기
+    getProjectInputs();
+}
+
+const getProjectInputs = () => {
+    const projectName = document.getElementById("projectName").value;
+    const projectDescription = document.getElementById("projectDescription").value;
+    const dev = $('select#selectDev').val()
+    const pl = $('select#selectPL').val()
+    const tester = $('select#selectTester').val()
+    
+    //확인
+    console.log(projectName, projectDescription, dev, pl, tester);
+
+    return {
+        projectName,
+        projectDescription
+    }
 }
