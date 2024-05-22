e6921c21c1f2894ea4a7a0a57231f3bcc3ce9662
suzzang2 <subin030102@naver.com>
2024-05-22 22:52:13 +0900

    FEAT: longinpage 쿠키 받아오기 설정

--- loginpage.js
+++ loginpage.js
@@ -1,35 +1,36 @@
 const baseURL = "https://jjapra.r-e.kr";
 
-function login() {
-    const idInput = document.getElementById("userID");
-    const passwordInput = document.getElementById("userPassword");
-    const id = idInput.value;
-    const password = passwordInput.value;
+ function login() {
+     const idInput = document.getElementById("userID");
+     const passwordInput = document.getElementById("userPassword");
+     const id = idInput.value;
+     const password = passwordInput.value;
 
-    fetch(baseURL+"/login", {
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'text/plain'
-        },
-        body: JSON.stringify({
-            id : id,
-            password : password
-        })
-    })
-    .then((response)=> {
-        console.log(response.text());
-        if(response.status == 200){
-            alert("Login Successed");
-            window.location.href="./ProjectList.html"
-        }
-        else{
-            alert("Please check your ID or Password.");
-            idInput.value = ""; 
-            passwordInput.value = "";
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+     fetch(baseURL+"/login", {
+         method: 'POST', 
+         credentials: 'include',  // 쿠키를 포함하도록 설정
+         headers: {
+             'Content-Type': 'application/json',
+             'Accept': 'text/plain'
+         },
+         body: JSON.stringify({
+             id : id,
+             password : password
+         })
+     })
+     .then((response)=> {
+         console.log(response.text());
+         if(response.status == 200){
+             alert("Login Successed");
+            //  window.location.href="./ProjectList.html"
+         }
+         else{
+             alert("Please check your ID or Password.");
+             idInput.value = ""; 
+             passwordInput.value = "";
+         }
+     })
+     .catch((error) => {
+         console.error('Error:', error);
+     });
 }
