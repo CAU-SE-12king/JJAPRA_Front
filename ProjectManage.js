cb2bd79b0302baf349c962dc61dc00934e6aba82
SongMinhyuk <dnfldpden32@gmail.com>
2024-05-26 20:52:17 +0900

    FEAT: projectmanage API 연동

--- ProjectManage.js
+++ ProjectManage.js
@@ -5,8 +5,112 @@ const formElement = document.querySelector("form");
 formElement.addEventListener("submit", saveIssue);
 formElement.addEventListener("reset", closemodal);
 
+const projectID = 1;
+
+// fetch(`https://jjapra.r-e.kr/projects/${projectID}/issues`, {
+//   credentials: "include", // 쿠키포함
+// })
+//   .then((response) => {
+//     response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     if (data.length == 0) {
+//     } else {
+//     }
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     //대체
+//   });
+
+const id = "test1";
+const password = "test1";
+const baseURL = "https://jjapra.r-e.kr";
+
+const login = async () => {
+  await fetch(baseURL + "/login", {
+    method: "POST",
+    credentials: "include", // 쿠키를 포함하도록 설정
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      password: password,
+    }),
+  })
+    .then((response) => {
+      console.log(response.json());
+      if (response.status == 200) {
+        //  window.location.href="./ProjectList.html"
+      } else {
+        alert("세션이 만료되어 로그인화면으로 돌아갑니다.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+let local = "./test.json";
+const getData = async () => {
+  await login();
+  fetch(baseURL + "/projects/" + projectID + "/issues", {
+    credentials: "include", // 쿠키포함
+  })
+    // 가져온 데이터를 JSON 형식으로 변환
+    .then((response) => response.json())
+    // 변환된 JSON 데이터를 콘솔에 출력
+    .then((response) => {
+      console.log(response);
+
+      const newIssuesSectionElement = document.getElementById("new");
+      const assignedIssuesSectionElement = document.getElementById("assigned");
+      const resolvedIssuesSectionElement = document.getElementById("resolved");
+      const fixedIssuesSectionElement = document.getElementById("fixed");
+      const closedIssuesSectionElement = document.getElementById("closed");
+
+      response.map((data) => {
+        //data 배열들을 돌면서 요소들 출력
+        //wrapper 생성
+        const liElement = document.createElement("li");
+        const aElement = document.createElement("a");
+        liElement.appendChild(aElement);
+        aElement.setAttribute(
+          "href",
+          `./issueDetail.html/?issueId=${data.issueId}&projectId=${data.projectId}`
+        );
+        liElement.setAttribute("id", `${data.issueId}`);
+        aElement.innerHTML = `${data.title}`;
+
+        switch (data.status) {
+          case "NEW":
+            newIssuesSectionElement.children[1].appendChild(liElement);
+            break;
+          case "assigned":
+            assignedIssuesSectionElement.children[1].appendChild(liElement);
+            break;
+          case "resolved":
+            resolvedIssuesSectionElement.children[1].appendChild(liElement);
+            break;
+          case "fixed":
+            fixedIssuesSectionElement.children[1].appendChild(liElement);
+            break;
+          case "closed":
+            closedIssuesSectionElement.children[1].appendChild(liElement);
+            break;
+          default:
+            //진행상태가 없는거? 오류
+            break;
+        }
+      });
+    });
+};
+
+getData();
+
 [...createIssuebtnElements].forEach(function (element) {
-  console.log(element);
   element.addEventListener("click", showmodal);
 });
 
