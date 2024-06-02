69bb9fbea54245feb17c7233436be8b756c6155a
SongMinhyuk <dnfldpden32@gmail.com>
2024-06-02 16:26:41 +0900

    MODIFIED: Header 적용

--- ProjectManage.js
+++ ProjectManage.js
@@ -233,3 +233,68 @@ function makeIssue(formData) {
 }
 setElementsbyRole(userRole);
 getData();
+
+function displayUsername() {
+  console.log("displayUsername() called");
+  const username = localStorage.getItem("username"); // localStorage에서 사용자 이름 가져오기
+  if (username) {
+    document.querySelector("#profile span").textContent = username; // 사용자 이름을 페이지에 표시
+  }
+}
+
+function logOut() {
+  const confirmed = confirm("Are you sure you want to log out?");
+  if (confirmed) {
+    localStorage.removeItem("username"); // 사용자 이름 삭제
+    localStorage.removeItem("TOKEN"); // 토큰 삭제!!
+    console.log("logout");
+    location.href = "./loginpage.html";
+  } else {
+    console.log("Logout canceled");
+  }
+}
+function logIn() {
+  location.href = "./loginpage.html";
+}
+
+function toggleLoginLogoutButtons() {
+  const username = localStorage.getItem("username");
+  const loginBtn = document.getElementById("loginBtn");
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  if (username) {
+    // 사용자가 로그인한 경우
+    loginBtn.style.display = "none"; // 로그인 버튼 숨기기
+    logoutBtn.style.display = "inline-block"; // 로그아웃 버튼 보이기
+  } else {
+    // 사용자가 로그인하지 않은 경우
+    loginBtn.style.display = "flex"; // 로그인 버튼 보이기
+    logoutBtn.style.display = "inline-block"; // 로그아웃 버튼 숨기기
+  }
+}
+
+//sidebar test
+document
+  .getElementById("toggleSidebarBtn")
+  .addEventListener("click", function () {
+    const sidebar = document.getElementById("sidebar");
+    const content = document.getElementById("mainContainer");
+    if (sidebar.style.width === "170px") {
+      sidebar.style.width = "0";
+      content.style.marginLeft = "100px";
+    } else {
+      sidebar.style.width = "170px";
+      content.style.marginLeft = "200px";
+    }
+  });
+
+// 프로필 누르면 해당 계정에만 해당하는 issue 페이지로 이동
+function openUserIssues() {
+  // window.location.href = "./UserIssues.html";
+  const username = localStorage.getItem("username");
+  if (username) {
+    window.location.href = "./UserIssues.html";
+  } else {
+    alert("Please Log in first"); // 로그인하라는 메시지
+  }
+}
